7e0ae4c53a6c67e4110d22872437134098062f15
Unknown <paosalcedo@gmail.com>
2017-03-29 00:12:53 -0400

    Added sample list

---
+++ Code Lab Midterm/Assets/NameGeneratorScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NameGeneratorScript : MonoBehaviour {
+
+	public List<string> names;
+	
+	void Start ()
+	{
+		names.Add ("Paolo");
+		names.Add ("Jono");
+		names.Add ("Macky");
+		names.Add ("Jan");
+		names.Add ("Mario");
+		names.Add ("Marco");
+
+		string[] nameArray = names.ToArray ();
+	
+		for (int i = 0; i < names.Count; i++) {
+			Debug.Log(names[i]);
+		}
+	}
+	
+	void Update ()
+	{
+		
+	}
+}
